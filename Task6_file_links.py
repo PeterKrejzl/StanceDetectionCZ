e4d0c157ce425e63c86b5b894b14623f1926cd52
peterkrejzl <pkrejzl@seznam.cz>
2016-12-29 21:29:53 +0100

    links to the data files

---
+++ Task6_file_links.py
@@ -0,0 +1,22 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+logging.root.setLevel(level=logging.INFO)
+
+
+
+training_data_filename = 'data/koureni.txt'
+
+
+stop_words = 'stopwords/czech_stop_words_out.txt'
+
+
+cs_n = 'slovniky/cs_n_out.def'
+cs_p = 'lovniky/cs_p_out.def'
+
+
+
+idnes_comments = "idnes/noentity_out.txt"
+
+
+
